2ee58c89f90715b1ed29b7be789eff91713caffc
Eduardo Oliveira Alves <eduardo.alves@icc.org.br>
2023-08-24 17:59:02 -0300

    id_generator

--- script.c
+++ script.c
@@ -1,5 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 
-void main()
+void id_generator(int *id)
 {
-    printf("Hello World!");
+    *id = rand();
+}
+
+struct Pessoa {
+    unsigned int id;
+    char nome[30];
+    char email[50];
+};
+
+void main() {
+    struct Pessoa pessoa;
+    unsigned int *id = &pessoa.id;
+    
+    printf("\nDigite seu nome: ");
+    scanf("%s", &pessoa.nome);
+    
+    printf("\nDigite seu email: ");
+    scanf("%s", &pessoa.email);
+    
+    bool isEmail = false;
+    
+    for (int i = 0; i < strlen(pessoa.email); i++)
+    {    
+        if (pessoa.email[i] == '@')
+        {
+            isEmail = true;
+        }
+    }
+    
+    if (isEmail == false)
+    {
+        printf("\nEmail inválido");
+        return;
+    }
+    
+    id_generator(id);
+    
+    printf("\n\nId: %u", pessoa.id);
+    printf("\nNome: %s", pessoa.nome);
+    printf("\nEmail: %s", pessoa.email);
 }
